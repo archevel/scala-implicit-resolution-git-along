e869ed79e9c63a848e081eaa04354cf5eaef38e0
Emil Hellman <emil.hellman@gmail.com>
2015-09-08 20:19:51 +1200

    example 3 - inheritance

--- src/main/scala/example/example.scala
+++ src/main/scala/example/example.scala
@@ -4,11 +4,17 @@ case class Hay(n:Int)
 
 object Hay {
     implicit def g = Hay(1)
-    implicit val h = Hay(2)
-    implicit object Straw extends Hay(3)
 }
 
-object Example {
+class Foo {
+    implicit val fooHay = Hay(2)
+}
+
+class Bar extends Foo {
+    implicit val barHay = Hay(3)
+}
+
+object Example extends Bar {
     def notWar(implicit h:Hay) = println(h)
     def printHay = notWar
 }
